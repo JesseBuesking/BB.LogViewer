cdb09a787e26324652932ca4da7b3a01bbcc04ad
JesseBuesking <JesseBuesking@gmail.com>
2012-11-07 00:49:39 -0800

    initial commit.  nothing amazing, but it technically works

---
+++ BB.LogViewer.Console/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace BB.LogViewer.Console
+{
+    public class Program
+    {
+        public static Timer checkLogsForNew;
+
+        public static DateTime lastGeneratedDT;
+
+        public static string logName;
+
+        public static string machineName;
+
+        public static HashSet<string> sources;
+
+        public static HashSet<EventLogEntryType> logEntryTypes;
+
+        static void Main(string[] args)
+        {
+            Program.logName = "Application";
+            Program.machineName = ".";
+            Program.sources = null;
+            Program.lastGeneratedDT = DateTime.MinValue;
+            Program.logEntryTypes = new HashSet<EventLogEntryType>
+            {
+                EventLogEntryType.FailureAudit,
+                EventLogEntryType.Error,
+                EventLogEntryType.Warning,
+                EventLogEntryType.SuccessAudit,
+                EventLogEntryType.Information
+            };
+
+            while (true)
+            {
+                var logs = Utilities.LogReader.GetLogs(Program.logName, Program.machineName, Program.sources,
+                Program.logEntryTypes, ref Program.lastGeneratedDT);
+
+                Utilities.LogReader.WriteLogsToConsole(logs);
+
+                // argh blocking!!!
+                Thread.Sleep(1000); // sleep for 1 second
+            }
+
+            //checkLogsForNew = new Timer(CheckLogsForNew, null, 0, (int)TimeSpan.FromSeconds(1).TotalMilliseconds);
+
+            //Thread.Sleep(10000000);
+        }
+
+        //static void CheckLogsForNew(object state)
+        //{
+        //    var logs = Utilities.LogReader.GetLogs(Program.logName, Program.machineName, Program.sources, 
+        //        Program.logEntryTypes, ref Program.lastGeneratedDT);
+
+        //    Utilities.LogReader.WriteLogsToConsole(logs);
+        //}
+    }
+}
---
+++ BB.LogViewer.Utilities/LogReader.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace BB.LogViewer.Utilities
+{
+    /// <summary>
+    /// Methods to read logs.
+    /// </summary>
+    public static class LogReader
+    {
+        /// <summary>
+        /// Get the specified logs.
+        /// </summary>
+        /// <param name="logName">The name of the log to use.</param>
+        /// <param name="machineName">The machine to use.</param>
+        /// <param name="sources">The sources to get logs from.</param>
+        /// <param name="logEntryTypes">The type of logs to get.</param>
+        /// <param name="lastGeneratedDT">The starting point in time to get logs from.</param>
+        public static IOrderedEnumerable<EventLogEntry> GetLogs(string logName, string machineName,
+            HashSet<string> sources, HashSet<EventLogEntryType> logEntryTypes, ref DateTime lastGeneratedDT)
+        {
+            DateTime minValue = lastGeneratedDT;
+
+            EventLog eventLog = new EventLog(logName, machineName);
+
+            var logs = eventLog.Entries.Cast<EventLogEntry>()
+                .Where(logEntry => minValue < logEntry.TimeGenerated)
+                .Where(logEntry => null == sources || sources.Contains(logEntry.Source, new InvariantCultureIgnoreCaseComparer()))
+                .Where(logEntry => null == logEntryTypes || logEntryTypes.Contains(logEntry.EntryType))
+                .OrderBy(logEntry => logEntry.TimeGenerated);
+
+            lastGeneratedDT = DateTime.UtcNow;
+
+            return logs;
+        }
+
+        /// <summary>
+        /// Get the last <see cref="limit"/> logs.
+        /// </summary>
+        /// <param name="logName">The name of the log to use.</param>
+        /// <param name="machineName">The machine to use.</param>
+        /// <param name="sources">The sources to get logs from.</param>
+        /// <param name="limit">How many logs to get.</param>
+        /// <param name="logEntryTypes">The type of logs to get.</param>
+        public static IOrderedEnumerable<EventLogEntry> Tail(string logName, string machineName, 
+            HashSet<string> sources, int limit, HashSet<EventLogEntryType> logEntryTypes)
+        {
+            EventLog eventLog = new EventLog(logName, machineName);
+
+            return eventLog.Entries.Cast<EventLogEntry>()
+                .Where(logEntry => null == sources || sources.Contains(logEntry.Source, new InvariantCultureIgnoreCaseComparer()))
+                .Where(logEntry => null == logEntryTypes || logEntryTypes.Contains(logEntry.EntryType))
+                .OrderByDescending(logEntry => logEntry.TimeGenerated)
+                .Take(limit)
+                .OrderByDescending(logEntry => logEntry.TimeGenerated);
+        }
+
+        /// <summary>
+        /// Writes the logs supplied to the console, coloring them.
+        /// </summary>
+        /// <param name="logs">The logs to write to the console.</param>
+        public static void WriteLogsToConsole(IOrderedEnumerable<EventLogEntry> logs)
+        {
+            var defaultColor = Console.ForegroundColor;
+            foreach (var log in logs)
+            {
+                switch (log.EntryType)
+                {
+                    case EventLogEntryType.FailureAudit:
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        break;
+                    case EventLogEntryType.SuccessAudit:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case EventLogEntryType.Information:
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                    case EventLogEntryType.Warning:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case EventLogEntryType.Error:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                }
+
+                Console.WriteLine("{0} -- {1} -- {2} -- {3} -- {4}", log.EntryType,
+                    log.MachineName, log.TimeGenerated, log.Source, log.Message);
+
+                Console.ForegroundColor = defaultColor;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Compares strings using StringComparison.InvariantCultureIgnoreCase.
+    /// </summary>
+    class InvariantCultureIgnoreCaseComparer : EqualityComparer<string>
+    {
+        /// <summary>
+        /// Override.
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        public override bool Equals(string s1, string s2)
+        {
+            return s1.Equals(s2, StringComparison.InvariantCultureIgnoreCase);
+        }
+        
+        /// <summary>
+        /// Override.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public override int GetHashCode(string s)
+        {
+            return base.GetHashCode();
+        }
+    }
+}
